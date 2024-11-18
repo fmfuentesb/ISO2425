c21c2a07b28ff09060bd08fec88bd89c3198fcbd
fmfuentesb <fmfuentesb01@iescastelar.com>
2024-11-18 10:35:07 +0100

    Add files via upload

---
+++ ProcesosPorLotes/media.bat
@@ -0,0 +1,12 @@
+@echo off
+rem Script de Media
+:: Autor:Francisco Manuel Fuentes Borrallo
+rem Fecha: 18-11-2024
+
+set num1=4
+set num2=6
+set num3=8
+
+set /A med=(num1 + num2 + num3) / 3
+
+echo La media de los numeros %num1%, %num2%, %num3% es %med%
---
+++ ProcesosPorLotes/mkprofile.bat
@@ -0,0 +1,15 @@
+@echo off
+rem Script de Prueba
+:: Autor:Francisco Manuel Fuentes Borrallo
+rem Fecha: 18-11-2024
+mkdir %USERPROFILE%\asir %USERPROFILE%\daw %USERPROFILE%\dam %USERPROFILE%\ceti %USERPROFILE%smr
+
+mkdir %USERPROFILE%\asir\docs %USERPROFILE%\asir\pract %USERPROFILE%\asir\examen
+
+mkdir %USERPROFILE%\dam\docs %USERPROFILE%\dam\pract %USERPROFILE%\dam\examen
+
+mkdir %USERPROFILE%\daw\docs %USERPROFILE%\daw\pract %USERPROFILE%\daw\examen
+
+mkdir %USERPROFILE%\ceti\docs %USERPROFILE%\ceti\pract %USERPROFILE%\ceti\examen
+
+mkdir %USERPROFILE%\smr\docs %USERPROFILE%\smr\pract %USERPROFILE%\smr\examen
